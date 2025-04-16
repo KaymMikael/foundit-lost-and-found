62262a746962ae6d88a26d8f78becec371828f09
Sero <kmcaltiz@gmail.com>
2025-04-16 11:57:50 +0800

    create instantdb perm and schema

---
+++ instant.schema.ts
@@ -0,0 +1,37 @@
+// Docs: https://www.instantdb.com/docs/modeling-data
+
+import { i } from "@instantdb/react";
+
+const _schema = i.schema({
+  entities: {
+    $files: i.entity({
+      path: i.string().unique().indexed(),
+      url: i.any(),
+    }),
+    $users: i.entity({
+      email: i.string().unique().indexed(),
+    }),
+    profiles: i.entity({
+      firstName: i.string(),
+      lastName: i.string(),
+      age: i.number(),
+      gender: i.string(),
+      createdAt: i.date(),
+    }),
+  },
+  links: {
+    profileUser: {
+      forward: { on: "profiles", has: "one", label: "$user" },
+      reverse: { on: "$users", has: "one", label: "profile" },
+    },
+  },
+  rooms: {},
+});
+
+// This helps Typescript display nicer intellisense
+type _AppSchema = typeof _schema;
+interface AppSchema extends _AppSchema {}
+const schema: AppSchema = _schema;
+
+export type { AppSchema };
+export default schema;
