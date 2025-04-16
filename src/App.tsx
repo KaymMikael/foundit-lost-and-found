9f4adf43c5f6e07fe6a1ea3924e658484462f6e2
Sero <kmcaltiz@gmail.com>
2025-04-16 11:59:43 +0800

    create login page and add it to route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
 
 const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </>
   );
