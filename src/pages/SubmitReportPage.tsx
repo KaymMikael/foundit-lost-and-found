b55f678f934f9b92ac7917b3c3782b42ce745026
Sero <kmcaltiz@gmail.com>
2025-04-21 16:46:01 +0800

    update submit report page

--- src/pages/SubmitReportPage.tsx
+++ src/pages/SubmitReportPage.tsx
@@ -1,7 +1,11 @@
+import SubmitReportForm from "@/components/submit-report-form";
+
 const SubmitReportPage = () => {
   return (
     <div className="max-w-7xl 2xl:mx-auto flex flex-col gap-4">
-      SubmitReportPage
+      <h2 className="text-2xl font-semibold">Submit Report</h2>
+      {/* Submit Report Form */}
+      <SubmitReportForm />
     </div>
   );
 };
