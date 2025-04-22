7127870217268b9a42f076ba330eddcfa0a5e9e4
Sero <kmcaltiz@gmail.com>
2025-04-22 08:55:45 +0800

    update: LostItemsPage.tsx import the items pets search bar

--- src/pages/LostItemsPage.tsx
+++ src/pages/LostItemsPage.tsx
@@ -1,7 +1,11 @@
+import ItemsPetsSearchBar from "@/components/items-pets-search-bar";
+
 const LostItemsPage = () => {
   return (
     <div className="max-w-7xl 2xl:mx-auto flex flex-col gap-4">
-      LostItemsPage
+      <h2 className="text-2xl font-semibold">Lost Items</h2>
+      {/* Search bar */}
+      <ItemsPetsSearchBar />
     </div>
   );
 };
