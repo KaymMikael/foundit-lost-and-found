e4b893a4d58bb1f70c4dfe8e93b5c442a1cd8050
Sero <kmcaltiz@gmail.com>
2025-04-23 17:06:31 +0800

    update: LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,14 +1,30 @@
 import PageHeader from "@/components/page-header";
 import PageLayout from "@/components/page-layout";
 import { Button } from "@/components/ui/button";
+import { useNavigate } from "react-router";
+import { toast } from "sonner";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const handleSignInClick = () => {
+    toast.success("Login Successful", {
+      description:
+        "You are now logged in and being redirected to the dashboard.",
+    });
+
+    // redirect to dashboard after 3 seconds
+    setTimeout(() => {
+      navigate("/dashboard");
+    }, 1000 * 3);
+  };
+
   return (
     <PageLayout>
       <PageHeader activePath="/login" />
       <div className="flex justify-center pt-10">
         {/* Redirect to dashboard after login */}
-        <Button>Sign in with Google</Button>
+        <Button className="cursor-pointer" onClick={handleSignInClick}>Sign in with Google</Button>
       </div>
     </PageLayout>
   );
