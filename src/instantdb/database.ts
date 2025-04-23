ce63382d5fae2fa94563cf0fc90e4c9f7f49a507
Sero <kmcaltiz@gmail.com>
2025-04-23 17:34:02 +0800

    uninstall instantdb

--- src/instantdb/database.ts
+++
@@ -1,7 +0,0 @@
-import { init } from "@instantdb/react";
-import config from "@/config/config";
-import schema from "@/../instant.schema";
-
-const db = init({ appId: config.APP_ID, schema: schema });
-
-export default db;
