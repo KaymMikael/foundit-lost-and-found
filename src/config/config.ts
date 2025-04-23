d3e803d24ea375e418638245ba7d5ca8663b20e8
Sero <kmcaltiz@gmail.com>
2025-04-23 17:45:18 +0800

    delete config

--- src/config/config.ts
+++
@@ -1,5 +0,0 @@
-const APP_ID = import.meta.env.VITE_INSTANT_APP_ID as string;
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID as string;
-const GOOGLE_CLIENT_NAME = import.meta.env.VITE_GOOGLE_CLIENT_NAME as string;
-
-export default { APP_ID, GOOGLE_CLIENT_ID, GOOGLE_CLIENT_NAME };
