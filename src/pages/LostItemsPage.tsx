85b61eca0874f63f348577f7315e99291ad1b960
Sero <kmcaltiz@gmail.com>
2025-04-24 09:01:20 +0800

    update: LostItemsPage.tsx

--- src/pages/LostItemsPage.tsx
+++ src/pages/LostItemsPage.tsx
@@ -1,11 +1,26 @@
 import ItemsPetsSearchBar from "@/components/items-pets-search-bar";
+import PostReportCard from "@/components/PostReportCard";
+import ReportsGridLayout from "@/components/reports-grid-layout";
+import useItemsReports from "@/hooks/useItemsReports";
 
 const LostItemsPage = () => {
+  const { lostItemsReports } = useItemsReports();
+
   return (
     <div className="max-w-7xl 2xl:mx-auto flex flex-col gap-4">
       <h2 className="text-2xl font-semibold">Lost Items</h2>
       {/* Search bar */}
       <ItemsPetsSearchBar />
+      {/* Lost items grid */}
+      <ReportsGridLayout>
+        {lostItemsReports.length ? (
+          lostItemsReports.map((data) => (
+            <PostReportCard userPost={data} key={data.post.id} />
+          ))
+        ) : (
+          <p className="mt-2 text-gray-400">No reports found</p>
+        )}
+      </ReportsGridLayout>
     </div>
   );
 };
