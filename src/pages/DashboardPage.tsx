852f4b8786fb945093020de0eaa7354b45d04472
Sero <kmcaltiz@gmail.com>
2025-04-26 06:22:21 +0800

    update: Dashboard.tsx import useUser and user and use user data for personal greeting

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,12 +1,14 @@
 import MyReportsSection from "@/components/MyReportsSection";
 import QuickStatsSummary from "@/components/quick-stats-summary";
+import useUser from "@/hooks/useUser";
 
 const DashboardPage = () => {
+  const {user} = useUser()
   return (
     <div className="max-w-7xl 2xl:mx-auto flex flex-col gap-4">
       {/* Personal Greeting */}
       <p className="text-2xl md:text-3xl font-semibold">
-        Hi User, welcome back!
+        Hi {`${user.firstName} ${user.lastName}`}, welcome back!
       </p>
       {/* Quick Stats Summary */}
       <QuickStatsSummary />
