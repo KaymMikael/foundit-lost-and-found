935e97d09a7bd7529c626f47fb7a1fc4538baa3a
Sero <kmcaltiz@gmail.com>
2025-04-28 06:39:32 +0800

    create: EditReportPage.tsx

---
+++ src/pages/EditReportPage.tsx
@@ -0,0 +1,9 @@
+const EditReportPage = () => {
+  return (
+    <div className="max-w-7xl 2xl:mx-auto flex flex-col gap-4">
+      EditReportPage
+    </div>
+  );
+};
+
+export default EditReportPage;
