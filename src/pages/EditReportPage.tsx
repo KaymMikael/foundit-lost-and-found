c79ce786e9204a627aec0732a49f81f3b153f423
Sero <kmcaltiz@gmail.com>
2025-04-28 08:33:58 +0800

    update: EditReportPage.tsx render report not found if the report does not exists otherwise render the form

--- src/pages/EditReportPage.tsx
+++ src/pages/EditReportPage.tsx
@@ -1,7 +1,27 @@
+import EditReportForm from "@/components/edit-report-form";
+import ReportNotFound from "@/components/report-not-found";
+import useReports from "@/hooks/useReports";
+import { UserPost } from "@/types/index.type";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+
 const EditReportPage = () => {
+  const { postId } = useParams();
+  const { posts } = useReports();
+  const [userPost, setUserPost] = useState<UserPost | undefined>();
+
+  useEffect(() => {
+    const foundPost = posts.find((p) => p.post.id === postId);
+    setUserPost(foundPost);
+  }, [postId, posts]);
+
+  if (!userPost) {
+    return <ReportNotFound />;
+  }
+
   return (
     <div className="max-w-7xl 2xl:mx-auto flex flex-col gap-4">
-      EditReportPage
+      <EditReportForm userPost={userPost}/>
     </div>
   );
 };
