45eded673c040a882bffc4897057774e3473a1b9
Sero <kmcaltiz@gmail.com>
2025-04-28 08:05:21 +0800

    update: report-not-found.tsx center p

--- src/components/report-not-found.tsx
+++ src/components/report-not-found.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/button";
 const ReportNotFound = () => {
   return (
     <div className="flex flex-col items-center">
-      <p className="text-red-400 text-2xl">Uh Oh! Report Does Not Exists</p>
+      <p className="text-red-400 text-2xl text-center">Uh Oh! Report Does Not Exists</p>
       <Button variant={"link"} asChild>
         <Link to={"/dashboard"}>Back to Dashboard</Link>
       </Button>
